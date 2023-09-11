5fe590704a33697b970099c66c8b7a736ca9b5cf
RenanDGomes <125320321+RenanDGomes@users.noreply.github.com>
2023-09-11 19:22:02 -0300

    Add files via upload

---
+++ Postagens.java
@@ -0,0 +1,4 @@
+// Gabriel de Melo Tizziani e Renan Delalibera Gomes :)
+public interface Postagens {
+    public void postar(Usuario usuario, String legenda);
+}
---
+++ UsuarioPremium.java
@@ -0,0 +1,22 @@
+// Gabriel de Melo Tizziani e Renan Delalibera Gomes :)
+public class UsuarioPremium extends Usuario {
+    private int valorMensalidade;
+
+    public UsuarioPremium(String nome, String senha, int numSeguidores, int valorMensalidade) {
+        super(nome, senha, numSeguidores);
+        this.valorMensalidade = valorMensalidade;
+    }
+
+    public int getValorMensalidade() {
+        return valorMensalidade;
+    }
+
+    public void setValorMensalidade(int valorMensalidade) {
+        this.valorMensalidade = valorMensalidade;
+    }
+
+    @Override
+    public void exibirTagUsuario() {
+        System.out.println("Usuário: " + this.getNome() + "| Seguidores: " + this.getNumSeguidores() + " PLUS ★");
+    }
+}
